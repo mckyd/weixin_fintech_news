a41e082937caef4d7d4b72523ec33de86ff7df2b
mckyd <mickeydu1218@hotmail.com>
2018-04-23 20:37:09 +0800

    .

--- weixin_fintech_news/spiders/weixin_fintech_news_spider.py
+++ weixin_fintech_news/spiders/weixin_fintech_news_spider.py
@@ -12,4 +12,13 @@ class WeixinFintechNewsSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        item = WeixinFintechNewsItem()
+        selector = Selector(response)
+        profile_href = selector.xpath('//*[@id="sogou_vr_11002301_box_0"]/div/div[2]/p[1]/a').extract()[0]
+
+        yield scrapy.Request(url=profile_href, callback=self.parse_profile())
+
+    def parse_profile(self, response):
+        push_lists = Selector(response).xpath('//*[@id="history"]').extract()[0]
+        for push_list in push_lists:
+            msg_card_bd = push_lists.xpath('//*').extract()[1]
+            #for msg in msg_card_bd:
